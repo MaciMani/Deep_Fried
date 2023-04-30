0752cd6cfba67a859b94a39f5174e695792f1ebf
NeilZKuN <joaogilmonteiro@gmail.com>
2023-04-30 14:10:11 +0100

    Adicionado: Primeira arma e teste com Target

---
+++ Assets/Scripts/ScriptableObjects/Gun Objects/Assault Rifle/Gun.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GunData gunData;
+    [SerializeField] private Transform muzzle;
+
+    float timeSinceLastShot;
+
+    private void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+    }
+
+    private bool CanShoot() => timeSinceLastShot > 1f / (gunData.fireRate / 60f);
+
+    public void Shoot() 
+    {
+        if (gunData.currentAmmo > 0)
+        {
+            if (CanShoot())
+            {
+                if (Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hitInfo, gunData.maxDistance))
+                {
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
+                    damageable?.TakeDamage(gunData.damage);
+                    Debug.Log("ola");
+                }
+            }
+
+            gunData.currentAmmo--;
+            timeSinceLastShot = 0;
+            //OnGunShot();
+        }
+    }
+
+    private void Update() 
+    {
+        timeSinceLastShot += Time.deltaTime;
+
+        Debug.DrawRay(muzzle.position, muzzle.forward * gunData.maxDistance);
+    }
+
+    
+    /*
+    private void OnGunShot() 
+    {
+        throw new NotImplementedException();
+    }
+    */
+    
+}
